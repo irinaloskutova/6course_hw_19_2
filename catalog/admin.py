acc33f03dc2afa3233b6dfd82fbccd2b0ec06652
irinaloskutova <l.irinka@mail.ru>
2023-05-10 00:20:17 +0500

    django_урок_3

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Student
+
+
+# admin.site.register(Student)
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    search_fields = ('first_name', 'last_name')
+    list_filter = ('is_active',)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=150, verbose_name="Имя")
+    last_name = models.CharField(max_length=150, verbose_name="Фамилия")
+    avatar = models.ImageField(upload_to='students/', verbose_name='аватар', **NULLABLE)
+
+    is_active = models.BooleanField(default=True, verbose_name="активный")
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'студент'
+        verbose_name_plural = 'студенты'
+        ordering = ('last_name', 'first_name')
+
